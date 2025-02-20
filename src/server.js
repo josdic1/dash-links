d793dae157cb8dd30161d4808e07224ba1dba73b
josdic1 <joshuadicker@gmail.com>
2025-02-20 13:38:35 -0500

    Add server.js and update start command

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
-import jsonServer from 'json-server'
-import cors from 'cors'
+const jsonServer = require('json-server');
+const cors = require('cors');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')  // Your db.json
-const middlewares = jsonServer.defaults()
-
-server.use(cors())  // Enable CORS for cross-origin requests
-server.use(middlewares)
-server.use(router)
-
+server.use(cors()); // Enable CORS
+server.use(middlewares);
+server.use(router);
 server.listen(3000, () => {
-   console.log('JSON Server is running')
-})
+   console.log('JSON Server is running');
+});
