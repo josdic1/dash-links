72e2603f954812aedea49744fbec839baf9e3171
josdic1 <joshuadicker@gmail.com>
2025-02-20 16:27:47 -0500

    added supabase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 
 const init = () => {
 
+  //supabase
+  const SUPABASE_URL = "https://dnuvbkjnikexrysohxik.supabase.co";
+  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRudXZia2puaWtleHJ5c29oeGlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwODYyNTMsImV4cCI6MjA1NTY2MjI1M30.qnafbgDghXIs2GB8uZqFUWXR_AO-94RiV7OKhSNccwk";
+
+
   //DOM elements
   const header = document.getElementById('header')
   const form = document.getElementById('form')
@@ -346,17 +351,24 @@ const init = () => {
     document.getElementById('inputPaid').checked = obj.paid;
   }
 
-
+  fetch(`${SUPABASE_URL}/rest/v1/links`, {
+    headers: {
+      "apikey": SUPABASE_KEY,
+      "Authorization": `Bearer ${SUPABASE_KEY}`
+    }
+  })
 
   //async fucntions
   //GET
 
   async function fetchLinks() {
     try {
-      const r = await fetch(`https://json-server-d6wz.onrender.com/links`)
-      if (!r.ok) {
-        throw new Error('GET: bad request')
-      }
+      const r = await fetch(`${SUPABASE_URL}/rest/v1/links`, {
+        headers: {
+          "apikey": SUPABASE_KEY,
+          "Authorization": `Bearer ${SUPABASE_KEY}`
+        }
+      })
       const data = await r.json()
       links = data
       renderList(data)
@@ -368,12 +380,17 @@ const init = () => {
 
   async function handleNewLink(newObj) {
     try {
-      const r = await fetch(`https://json-server-d6wz.onrender.com/links`, {
-        method: 'POST',
+      const r = await fetch(`${SUPABASE_URL}/rest/v1/links`, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "apikey": SUPABASE_KEY,
+          "Authorization": `Bearer ${SUPABASE_KEY}`,
+          "Content-Type": "application/json"
         },
-        body: JSON.stringify(newObj)
+        body: JSON.stringify({
+          name: "Google",
+          url: "https://google.com"
+        })
       })
       if (!r.ok) {
         throw new Error('POST: bad request')
@@ -385,8 +402,12 @@ const init = () => {
 
   async function handleDelete(obj) {
     try {
-      const r = await fetch(`https://json-server-d6wz.onrender.com/links/${obj.id}`, {
-        method: 'DELETE'
+      const r = await fetch(`${SUPABASE_URL}/rest/v1/links?id=eq.1`, {
+        method: "DELETE",
+        headers: {
+          "apikey": SUPABASE_KEY,
+          "Authorization": `Bearer ${SUPABASE_KEY}`
+        }
       })
       if (!r.ok) {
         throw new Error('DELETE: bad request')
@@ -397,7 +418,7 @@ const init = () => {
 
   async function handleUpdatedLink(updatedObj) {
     try {
-      const r = await fetch(`https://json-server-d6wz.onrender.com/links/${updatedObj.id}`, {
+      const r = await fetch(`http://localhost:3000/links/${updatedObj.id}`, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json'
