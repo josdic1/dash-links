e2ec4a353de11d97a687aea2286ae261870503ab
josdic1 <joshuadicker@gmail.com>
2025-02-20 13:27:25 -0500

    updatedAPI

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
+import jsonServer from 'json-server'
+import cors from 'cors'
+
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('db.json')  // Your db.json
 const middlewares = jsonServer.defaults()
 
-server.use(cors()) // Enable CORS
+server.use(cors())  // Enable CORS for cross-origin requests
 server.use(middlewares)
 server.use(router)
+
 server.listen(3000, () => {
    console.log('JSON Server is running')
 })
